6a3daebf04593e5e08adfda6f9af6fbb48cac266
Kenton Knelle <kken51915@gmail.com>
2026-02-02 03:27:27 -0500

    MVC Create Product (4) Push

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -11,5 +11,11 @@ public interface IProductRepository
     public IEnumerable<Product> GetAllProducts();
     Product GetProduct(int id);
     public void UpdateProduct(Product product);
+    
+    public void InsertProduct(Product productToInsert);
+    public IEnumerable<Category> GetCategories();
+    public Product AssignCategory();
+
+
 
 }
--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -18,6 +18,8 @@ public class Product
     public int CategoryID { get; set; }
     public int OnSale { get; set; }
     public int StockLevel  { get; set; }
+    
+    public IEnumerable<Category> Categories { get; set; }
 
 
 
