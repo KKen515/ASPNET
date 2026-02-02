6bbbbf7f9acf255906b17e0b7c31f98321dd81b8
Kenton Knelle <kken51915@gmail.com>
2026-02-02 10:11:07 -0500

    MVC Delete Product (5) Push

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -15,6 +15,8 @@ public interface IProductRepository
     public void InsertProduct(Product productToInsert);
     public IEnumerable<Category> GetCategories();
     public Product AssignCategory();
+    public void DeleteProduct(Product product);
+
 
 
 
